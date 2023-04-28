f784c8a1e8bb5febddc0381cc513872d80ccf7ea
Rafael <rafesilvadesouza@gmail.com>
2023-04-28 14:29:19 -0300

    first commit

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/whoami', (req, res) => {
+
+  res.json({
+    "ipaddress" : req.socket.remoteAddress,
+    "language": req.rawHeaders[7],
+    "software": req.rawHeaders[3],
+  })
+})
